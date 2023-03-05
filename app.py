0faae6d12d3ed9e560a08ffae2af0746da2f5391
Lord_Of_Victory <jnandanwar20@gmail.com>
2023-03-05 18:30:25 +0530

    v0.0

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html")
+
+@app.route("/user_dashboard")
+def user_dashboard():
+    return render_template("user_dashboard.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
